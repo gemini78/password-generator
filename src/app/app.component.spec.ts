07f6bbcc5a31288f819d910bf7e8168a2e0c9fcd
Stébou Mendy <stebou8mendy@gmail.com>
2024-03-15 11:24:37 +0100

    Unit test: click on button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,4 +16,24 @@ describe('AppComponent', () => {
 
         expect(article.textContent).toBe('Cliquez sur le bouton "Générer"')
     })
+
+    it('should changemessage when user clicks on generate button', async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AppComponent]
+        }).compileComponents();
+
+        const fixture = TestBed.createComponent(AppComponent);
+
+        fixture.detectChanges();
+
+        const button = fixture.nativeElement.querySelector('button');
+
+        button.click();
+
+        fixture.detectChanges();
+
+        const article = fixture.nativeElement.querySelector('article');
+
+        expect(article.textContent).toBe('MON_MOT_DE_PASSE')
+    })
 })
